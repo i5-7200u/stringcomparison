e72c45099a2c4d753d11a6e637d7c3b07200656e
i5-7200u <81111892+i5-7200u@users.noreply.github.com>
2021-08-25 06:11:21 +0300

    unimportant codes cleanup

---
+++ stringcomparison_1_3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+int chararraycomparison(char birchar[], char ikichar[])
+     {
+int a = 0;
+int b = 1;
+int char1len = strlen(birchar);
+int char2len = strlen(ikichar);
+if (char1len != char2len)
+ {
+return 0;
+ }
+else
+ {
+while(b>0){
+if(a != char1len){
+if (birchar[a] == ikichar[a]){
+
+}
+else{
+b = 0;
+return 0;
+}
+a++;
+}
+else{
+b = 0;
+return 1;
+}
+
+}
+ }
+      }
